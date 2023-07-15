d2b0c26fa4f6368b5b0530fae36186c830be245f
Ghassan Malke <138721734+gma1k@users.noreply.github.com>
2023-07-15 16:31:23 +0200

    Create exif_modify_02.sh

---
+++ security/exif/exif_modify_02.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+print_exif_data() {
+  local file="$1"
+  case $(file -b --mime-type "$file") in
+    image/jpeg|image/tiff|image/png|image/webp)
+      ;;
+    *)
+      echo "Unsupported image format: $(file -b --mime-type "$file")"
+      ;;
+  esac
+}
+
+modify_exif_data() {
+  local file="$1"
+  case $(file -b --mime-type "$file") in
+    image/jpeg|image/tiff|image/png|image/webp)
+      read -p "Enter the EXIF key to modify (e.g. Exif.Photo.DateTimeOriginal): " key
+      if exiv2 -g "$key" -Pv "$file" >/dev/null 2>&1; then
+        read -p "Enter the new value for $key: " value
+        exiv2 -M"set $key $value" "$file"
+        echo "EXIF data modified successfully."
+      else
+        echo "Invalid EXIF key: $key"
+      fi
+      ;;
+    *)
+      echo "Unsupported image format: $(file -b --mime-type "$file")"
+      ;;
+  esac
+}
+
+delete_exif_data() {
+  local file="$1"
+  case $(file -b --mime-type "$file") in
+    image/jpeg|image/tiff|image/png|image/webp)
+      exiv2 rm "$file"
+      echo "EXIF data deleted successfully."
+      ;;
+    *)
+      echo "Unsupported image format: $(file -b --mime-type "$file")"
+      ;;
+  esac
+}
+
+read -p "Enter a picture file name: " file
+
+print_exif_data "$file"
+
+read -p "Do you want to modify or delete the EXIF data? (m/d/n): " choice
+
+case $choice in
+  m|M)
+    modify_exif_data "$file"
+    ;;
+  d|D)
+    delete_exif_data "$file"
+    ;;
+  n|N)
+    echo "No changes made."
+    ;;
+  *)
+    echo "Invalid choice."
+    ;;
+esac
