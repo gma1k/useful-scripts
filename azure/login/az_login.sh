85323283947b64e46895655245ffe0429b4ff207
Ghassan Malke <138721734+gma1k@users.noreply.github.com>
2023-07-16 08:53:55 +0200

    Update az_login.sh

--- azure/login/az_login.sh
+++ azure/login/az_login.sh
@@ -1,29 +1,23 @@
-# A function to login with a user account
 login_user() {
   az login
 }
 
-# A function to login with a service principal
 login_sp() {
   read -p "Enter the service principal name: " sp_name
   read -sp "Enter the service principal password: " sp_password
   echo
 
-  # Create a service principal
   az ad sp create-for-rbac --name $sp_name --password $sp_password
 
-  # Get the service principal ID and tenant ID
   sp_id=$(az ad sp show --id http://$sp_name --query appId --output tsv)
   tenant_id=$(az account show --query tenantId --output tsv)
 
-  # Configure the service principal credentials
   az configure --defaults group=MyResourceGroup
   az configure --defaults username=$sp_id
   az configure --defaults password=$sp_password
   az configure --defaults tenant=$tenant_id
 }
 
-# Start a loop
 while true; do
   read -p "How do you want to login? (ua) User account (sp) Service principal (q) Quit: " choice
 
