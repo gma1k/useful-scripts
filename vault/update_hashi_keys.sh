5edc204446c9e590f83a8347bec11bf90c2bf1bc
Ghassan Malke <138721734+gma1k@users.noreply.github.com>
2023-07-16 09:17:11 +0200

    Update update_hashi_keys.sh

--- vault/update_hashi_keys.sh
+++ vault/update_hashi_keys.sh
@@ -6,7 +6,6 @@ VAULT_ADDR="https://vault.example.com"
 read_password() {
   local prompt=$1
   local password=""
-  # Use read -s to suppress the input
   read -s -p "$prompt" password
   echo "$password"
 }
@@ -26,21 +25,17 @@ validate_token() {
 VAULT_TOKEN=$(read_password "Enter the vault token: ")
 echo ""
 
-# Validate the vault token and ask for a new one if invalid
 while ! validate_token $VAULT_TOKEN; do
   VAULT_TOKEN=$(read_password "Enter a new vault token: ")
   echo ""
 done
 
-# A function to update a key in Hashi Vault
 update_key() {
   local key_name=$1
   local password=$2
-  # Use the variables for the vault address and token
   curl -X POST -H "X-Vault-Token: $VAULT_TOKEN" -d "{\"value\": \"$password\"}" $VAULT_ADDR/v1/secret/data/$key_name
 }
 
-# A function to validate the key name
 validate_key() {
   local key_name=$1
   local response=$(curl -s -o /dev/null -w "%{http_code}" -H "X-Vault-Token: $VAULT_TOKEN" $VAULT_ADDR/v1/secret/data/$key_name)
@@ -53,7 +48,6 @@ validate_key() {
   fi
 }
 
-# A function to compare two passwords
 compare_passwords() {
   local password1=$1
   local password2=$2
@@ -66,26 +60,21 @@ compare_passwords() {
   fi
 }
 
-# Ask for the number of keys to update
 read -p "How many keys do you want to update? " num_keys
 
-# Use a regular expression to check if the input a positive integer
 while ! [[ "$num_keys" =~ ^[1-9][0-9]*$ ]]; do
   echo "Invalid number of keys: $num_keys"
   read -p "Enter a positive integer: " num_keys
 done
 
-# A loop to ask for key names and passwords from user input and update them in Hashi Vault
 for ((i=1; i<=num_keys; i++)); do
 
   read -p "Enter a key name for key #$i: " key_name
 
-  # Validate the key name and ask for a new one if invalid
   while ! validate_key $key_name; do
     read -p "Enter a new key name for key #$i: " key_name
   done
 
-  # Ask for password and compare
   password1=$(read_password "Enter a password for $key_name: ")
   echo ""
   password2=$(read_password "Re-enter the password for $key_name: ")
