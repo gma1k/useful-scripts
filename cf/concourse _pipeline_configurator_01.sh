8a27e4c10b50b46e7049048b56710bd587b17474
Ghassan Malke <138721734+gma1k@users.noreply.github.com>
2023-07-16 08:59:17 +0200

    Update concourse _pipeline_configurator_01.sh

--- cf/concourse _pipeline_configurator_01.sh
+++ cf/concourse _pipeline_configurator_01.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 set -e
 
 echo "Please enter the target name. This is a name that you choose to identify your concourse instance."
@@ -9,16 +11,12 @@ read pipeline_name
 echo "Please enter the pipeline file. This is the path to the YAML file that defines your pipeline configuration."
 read pipeline_file
 
-# Log in
 fly login -t $target_name -c $concourse_url || { echo "Failed to log in to $target_name. Please check your credentials and url."; exit 1; }
 
-# Set pipeline
 fly set-pipeline -t $target_name -p $pipeline_name -c $pipeline_file || { echo "Failed to set pipeline $pipeline_name. Please check your pipeline file."; exit 1; }
 
-# Unpause pipeline
 fly unpause-pipeline -t $target_name -p $pipeline_name || { echo "Failed to unpause pipeline $pipeline_name."; exit 1; }
 
-# Trigger the first job
 first_job=$(fly jobs -t $target_name -p $pipeline_name | awk 'NR==2 {print $1}')
 fly trigger-job -t $target_name -j $pipeline_name/$first_job || { echo "Failed to trigger job $first_job."; exit 1; }
 
