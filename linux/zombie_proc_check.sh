176db5c8cf7f8c063ff1053f63fb979e4936fd01
Ghassan Malke <138721734+gma1k@users.noreply.github.com>
2023-07-16 09:19:48 +0200

    Update zombie_proc_check.sh

--- linux/zombie_proc_check.sh
+++ linux/zombie_proc_check.sh
@@ -5,19 +5,15 @@ if [ $(id -u) -ne 0 ]; then
   exit 1
 fi
 
-# Get the current date and time
 now=$(date +"%Y-%m-%d %H:%M:%S")
 
-# Get the zombie processes
 zombies=$(ps -eo pid,ppid,state,cmd | awk '$3=="Z"')
 
-# Check if there are any zombies
 if [ -z "$zombies" ]; then
   echo "No zombie processes found at $now"
 else
   echo "Found zombie processes at $now:"
   echo "$zombies"
-  # Ask the user what they want to do
   read -p "Do you want to force kill all zombie processes (a), kill them one by one (b), or specify a process ID to kill (c)? (a/b/c) " answer
   case $answer in
     a)
@@ -25,7 +21,6 @@ else
       for name in $znames; do
         echo "Killing all processes named $name"
         killall -9 $name
-        # Check if the kill was successful
         if [ $? -eq 0 ]; then
           echo "Successfully killed all processes named $name"
         else
@@ -34,7 +29,6 @@ else
       done
       ;;
     b)
-      # Loop through the zombies and KILL THEM!
       while read -r pid ppid state cmd; do
         pcmd=$(ps -p $ppid -o cmd --no-headers)
         echo "Killing parent process $ppid ($pcmd) of zombie $pid"
