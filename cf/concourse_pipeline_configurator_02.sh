f3fe178b30a46874f215c31eaf8ef0d7b62f11b7
Ghassan Malke <138721734+gma1k@users.noreply.github.com>
2023-07-16 09:00:35 +0200

    Update concourse_pipeline_configurator_02.sh

--- cf/concourse_pipeline_configurator_02.sh
+++ cf/concourse_pipeline_configurator_02.sh
@@ -4,7 +4,6 @@ set -e
 
 options=("List recent builds" "Stream logs of a job" "Connect to a container" "Check for new versions of a resource" "Validate pipeline configuration" "Run a local task file")
 
-# Print the options
 echo "Please choose one of the following options:"
 for i in "${!options[@]}"; do
   echo "$((i+1))) ${options[$i]}"
@@ -12,41 +11,38 @@ done
 
 read -p "Enter your choice: " choice
 
-# Validate the user input
 if [[ $choice -lt 1 || $choice -gt ${#options[@]} ]]; then
   echo "Invalid choice. Please enter a number between 1 and ${#options[@]}."
   exit 1
 fi
 
-# Execute the corresponding command based on the user input
 case $choice in
-  1) # List recent builds
+  1)
     fly builds -t <target-name> || { echo "Failed to list recent builds. Please check your target name and connection."; exit 1; }
     ;;
-  2) # Stream logs of a job
+  2)
     read -p "Enter the pipeline name: " pipeline_name
     read -p "Enter the job name: " job_name
     fly watch -t <target-name> -j $pipeline_name/$job_name || { echo "Failed to stream logs of $job_name. Please check your pipeline name, job name and connection."; exit 1; }
     ;;
-  3) # Connect to a container
+  3)
     read -p "Enter the pipeline name: " pipeline_name
     read -p "Enter the job name: " job_name
     fly intercept -t <target-name> -j $pipeline_name/$job_name || { echo "Failed to connect to a container. Please check your pipeline name, job name and connection."; exit 1; }
     ;;
-  4) # Check for new versions of a resource
+  4)
     read -p "Enter the pipeline name: " pipeline_name
     read -p "Enter the resource name: " resource_name
     fly check-resource -t <target-name> -r $pipeline_name/$resource_name || { echo "Failed to check for new versions of $resource_name. Please check your pipeline name, resource name and connection."; exit 1; }
     ;;
-  5) # Validate pipeline configuration
+  5)
     read -p "Enter the pipeline file: " pipeline_file
     fly validate-pipeline -t <target-name> -c $pipeline_file || { echo "Failed to validate pipeline configuration. Please check your target name, pipeline file and connection."; exit 1; }
     ;;
-  6) # Run a local task file
+  6)
     read -p "Enter the task file: " task_file
     fly execute -t <target-name> -c $task_file || { echo "Failed to run local task file. Please check your target name, task file and connection."; exit 1; }
     ;;
 esac
 
-# Print a success message
 echo "Command executed successfully."
