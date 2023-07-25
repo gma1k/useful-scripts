4523f051bd1f653b9347e357fb5bfacc9940cc5e
Ghassan Malke <138721734+gma1k@users.noreply.github.com>
2023-07-25 14:23:43 +0200

    Create check_logs.sh (#19)

---
+++ linux/check_logs.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+function log_search() {
+  echo "Choose an option:"
+  echo "1) Search for errors in log files"
+  echo "2) Search for warnings in log files"
+  echo "3) Search for both errors and warnings in log files"
+  echo "4) Quit"
+
+  read -p "Enter your choice: " choice
+
+  case $choice in
+    1)
+      read -p "Enter the directory path of the log files: " dir_path
+      if [ -d "$dir_path" ]; then
+        read -p "Enter the keyword to grep on: " keyword
+        find "$dir_path" -type f \( -name "*.log" -o -name "*.log.gz" \) | while read file; do
+          if [[ $file == *.gz ]]; then
+            zgrep -iE "error.*$keyword|$keyword.*error" "$file"
+          else
+            grep -iE "error.*$keyword|$keyword.*error" "$file"
+          fi          
+        done        
+      else        
+        echo "$dir_path is not a valid directory path or does not exist"
+      fi      
+      ;;
+    2)
+      read -p "Enter the directory path of the log files: " dir_path      
+      if [ -d "$dir_path" ]; then        
+        read -p "Enter the keyword to grep on: " keyword        
+        find "$dir_path" -type f \( -name "*.log" -o -name "*.log.gz" \) | while read file; do          
+          # Check if the file is zipped or not          
+          if [[ $file == *.gz ]]; then            
+            zgrep -iE "warning.*$keyword|$keyword.*warning" "$file"          
+          else            
+            grep -iE "warning.*$keyword|$keyword.*warning" "$file"          
+          fi          
+        done        
+      else        
+        echo "$dir_path is not a valid directory path or does not exist"      
+      fi      
+      ;;
+    3) 
+      read -p "Enter the directory path of the log files: " dir_path      
+      if [ -d "$dir_path" ]; then        
+        read -p "Enter the keyword to grep on: " keyword        
+        find "$dir_path" -type f \( -name "*.log" -o -name "*.log.gz" \) | while read file; do          
+          if [[ $file == *.gz ]]; then            
+            zgrep -iE "(error|warning).*$keyword|$keyword.*(error|warning)" "$file"          
+          else            
+            grep -iE "(error|warning).*$keyword|$keyword.*(error|warning)" "$file"          
+          fi          
+        done        
+      else        
+        echo "$dir_path is not a valid directory path or does not exist"      
+      fi      
+      ;;
+    4)
+      echo "Bye!"      
+      exit 0      
+      ;;
+    *)
+      echo "Invalid choice, please try again"      
+      log_search      
+      ;;
+  esac  
+}
+
+log_search
