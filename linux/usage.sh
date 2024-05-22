e38898b4a50bc8102f4efcf77578b14c791a3d86
Ghassan Malke <138721734+gma1k@users.noreply.github.com>
2024-05-22 09:29:08 +0200

    Update usage.sh

--- linux/usage.sh
+++ linux/usage.sh
@@ -5,10 +5,10 @@ convert_kb() {
   local kb=$1
   # If the value is greater than or equal to 1 GB, use GB, otherwise use MB
   if [[ $kb -ge 1048576 ]]; then
-    # Convert to GB and add unit
+    # Convert to GB
     echo $(awk -v k=$kb 'BEGIN {printf "%.2f GB", k / (1024 * 1024)}')
   else
-    # Convert to MB and add unit
+    # Convert to MB
     echo $(awk -v k=$kb 'BEGIN {printf "%.2f MB", k / 1024}')
   fi
 }
@@ -17,13 +17,13 @@ read -p "Enter the path of user folder on ftp: " user_path
 
 read -p "Enter the path of ftp file system: " ftp_path
 
-# Get the disk usage of user folder in kilobytes using du command
+# Get the disk usage of user folder
 user_usage_kb=$(du -sk $user_path | awk '{print $1}')
 
-# Get the available space of ftp file system in kilobytes using df command
+# Get the available space of ftp file system
 ftp_avail_kb=$(df -k $ftp_path | awk 'NF == 6 {print $4} NF == 5 {print $3}')
 
-# Convert kilobytes to megabytes or gigabytes
+# Convert KB to MB or GB
 user_usage=$(convert_kb $user_usage_kb)
 ftp_avail=$(convert_kb $ftp_avail_kb)
 
