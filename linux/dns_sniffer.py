adf6c0d59668cc35566a3a28be69761c79c99fb0
Ghassan Malke <138721734+gma1k@users.noreply.github.com>
2025-11-14 18:24:31 +0100

    Editing dns_sniffer.py (#32)

--- linux/dns_sniffer.py
+++ linux/dns_sniffer.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
+
 from scapy.all import sniff, DNS, DNSQR, IP
 import sys
 
 def detect_iface():
     if len(sys.argv) > 1:
         return sys.argv[1]
-    # fall back to your known NIC
     return "enp4s0"
 
 iface = detect_iface()
-print(f"[+] Listening for DNS traffic on {iface} (UDP port 53)")
+print(f"[+] Listening for DNS traffic on {iface}")
 
 def handle_packet(pkt):
-    # We only care about DNS queries over UDP/53
     if pkt.haslayer(DNS) and pkt.haslayer(DNSQR):
         ip = pkt[IP].src if pkt.haslayer(IP) else "unknown"
         dns = pkt[DNS]
