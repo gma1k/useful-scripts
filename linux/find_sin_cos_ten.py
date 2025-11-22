f65f5af3e3f9cc29ddd0447504cbc2a224a74044
Ghassan Malke <138721734+gma1k@users.noreply.github.com>
2025-11-22 11:42:18 +0100

    Add sine, cosine, and tangent calculations

---
+++ linux/find_sin_cos_ten.py
@@ -0,0 +1,12 @@
+import math
+ 
+a = math.pi/6
+ 
+print ("The value of sine of pi/6 is : ", end="")
+print (math.sin(a))
+ 
+print ("The value of cosine of pi/6 is : ", end="")
+print (math.cos(a))
+ 
+print ("The value of tangent of pi/6 is : ", end="")
+print (math.tan(a))
